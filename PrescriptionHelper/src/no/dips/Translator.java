74769d0a855af59173eca10aca762f87c77fca6e
unknown <mbergdal@gmail.com>
2014-08-19 23:11:34 +0200

    Added HelloWorld examples

--- PrescriptionHelper/src/no/dips/Translator.java
+++ PrescriptionHelper/src/no/dips/Translator.java
@@ -3,6 +3,16 @@ package no.dips;
 public class Translator {
 
 	public String Translate(String input) {
-		return "Oops! Did not understand";
+		String translateString = "";
+		
+		if (input.contains(Character.toString('+'))){
+			String[] parts = input.split("\\+");
+			translateString = parts[0] + " units in the morning and " + parts[1] + " units in the evening";
+		}
+		else{
+			translateString =  "Oops! Did not understand";
+		}
+		
+		return translateString;
 	}
 }
