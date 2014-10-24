36c4ae05dd52e32cf5f3cc5e3977d47dcff1e161
mbergdal <mbergdal@gmail.com>
2014-10-24 10:59:03 +0200

    Added the QuestionEngine

---
+++ Java/ProgramingExercises4/src/hidden/Question.java
@@ -0,0 +1,20 @@
+package hidden;
+
+public class Question {
+	private String questionText;
+	private String answer;
+	
+	public Question(String questionText, String answer) {
+		this.questionText = questionText;
+		this.answer = answer;
+	}
+	
+	public String getQuestionText(){
+		return questionText;
+	}
+	
+	public String getAnswer(){
+		return answer;				
+	}
+	
+}
---
+++ Java/ProgramingExercises4/src/hidden/QuestionEngine.java
@@ -0,0 +1,28 @@
+package hidden;
+
+public class QuestionEngine {
+	private Question[] questions = new Question[2];
+	private int currentQuestionIndex = 0;
+	
+	public QuestionEngine(){
+		questions[0] = new Question("Hovedstaden i Norge?", "Oslo");
+		questions[1] = new Question("Hva heter det nye GUI rammeverket i Java?", "javafx");
+	}
+	
+	public String getNextQuestion() {
+		if(currentQuestionIndex < questions.length){
+			return questions[currentQuestionIndex].getQuestionText();
+		}else{
+			return null;
+		}
+	}
+
+	public boolean verifyAnswer(String answer) {
+		if (answer.toLowerCase().equals(questions[currentQuestionIndex].getAnswer().toLowerCase())){
+			currentQuestionIndex++;
+			return true;
+		}
+		
+		return false;
+	}
+}
